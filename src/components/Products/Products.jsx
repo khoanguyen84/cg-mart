612fbd3f2ddceb9e0536f51f1e2ca4dd085aa512
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-01-26 10:55:40 +0700

    add scroll down and fetching data

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,20 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Product from "./Product";
 import { ShoppingContext } from "../../context/shopping-context";
 import { fetchData } from "../../reducer/actions";
+import InfiniteScroll from "react-infinite-scroll-component";
 
+var limit = 8
+var totalRows = 0
 function Products() {
     const { state, dispatch } = useContext(ShoppingContext)
     const { products, filters: { searchText, status, brand, category, price } } = state
+    const [hasMore, setHasMore] = useState(true)
     useEffect(() => {
         async function getProductList() {
-            let res = await fetch('https://dummyjson.com/products?limit=100&skip=0')
+            let res = await fetch('https://dummyjson.com/products?limit=8&skip=0')
             let data = await res.json()
+            totalRows = data?.total
             dispatch(fetchData(data?.products))
         }
         getProductList()
@@ -37,14 +42,14 @@ function Products() {
         }
         if (price !== '0,0') {
             const [min, max] = price.split(',')
-            if(min !== max) {
+            if (min !== max) {
                 filteredProducts = filteredProducts.filter((p) => {
-                    let priceAfterDiscount = Math.round(p.price * (1 - p.discountPercentage* 0.01))
+                    let priceAfterDiscount = Math.round(p.price * (1 - p.discountPercentage * 0.01))
                     return priceAfterDiscount >= min && priceAfterDiscount < max
                 })
             }
             else {
-                filteredProducts = filteredProducts.filter((p) => Math.round(p.price * (1 - p.discountPercentage* 0.01)) >= min)
+                filteredProducts = filteredProducts.filter((p) => Math.round(p.price * (1 - p.discountPercentage * 0.01)) >= min)
             }
         }
 
@@ -52,17 +57,39 @@ function Products() {
     }
 
     const filteredProducts = queryProducts()
-    console.log(state.filters.price);
+
+    const fetchMoreData = async () => {
+        if(filteredProducts.length < totalRows){
+            limit += 8
+            let res = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=0`)
+            let data = await res.json()
+            dispatch(fetchData(data?.products))
+        }
+        else {
+            setHasMore(false)
+        }
+    }
+    
     return (
         <div className="py-2 d-flex flex-column justify-content-center">
             <h5>Products</h5>
-            <div className="row">
-                {
-                    filteredProducts?.map((product) => (
-                        <Product key={product.id} product={product} />
-                    ))
-                }
-            </div>
+            <InfiniteScroll
+                dataLength={filteredProducts.length}
+                next={fetchMoreData}
+                hasMore={hasMore}
+                loader ={<p>Loading...</p>}
+                endMessage = {<p>You have seen it all!</p>}
+                style={{overflow: 'hidden'}}
+                scrollThreshold={'100px'}
+            >
+                <div className="row">
+                    {
+                        filteredProducts?.map((product) => (
+                            <Product key={product.id} product={product} />
+                        ))
+                    }
+                </div>
+            </InfiniteScroll>
         </div>
     )
 }
---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+export default function Spinner() {
+    return (
+        <div className="spinner">
+            <div className="spinner-border text-success" role="status">
+            </div>
+            <div className="spinner-border text-danger" role="status">
+            </div>
+            <div className="spinner-border text-warning" role="status">
+            </div>
+        </div>
+    )
+}
